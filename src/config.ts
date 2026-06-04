f0824e686f6681f44830282f1947b0f213011450
hanifkf12 <hanifkhoirulfathan@gmail.com>
2026-06-04 19:04:13 +0700

    xxccc

--- src/config.ts
+++ src/config.ts
@@ -3,7 +3,7 @@ dotenv.config();
 
 function req(k: string): string {
   const v = process.env[k];
-  if (!v) throw new Error(`Missing xxenv var: ${k}`);
+  if (!v) throw new Error(`Missing xxxxxxxenv var: ${k}`);
   return v;
 }
 function opt(k: string, d: string): string { return process.env[k] ?? d; }
